3ea1b0dac6e8e39f7d98e11804b7b6a16d48cb6e
Starkeae <149506827+Starkeae@users.noreply.github.com>
2025-07-09 22:49:09 +0330

    Update __init__.py

--- locales/__init__.py
+++ locales/__init__.py
@@ -1,1 +1,11 @@
+from .fa import FA_MESSAGES
 
+MESSAGES = {
+    'fa': FA_MESSAGES
+}
+
+def get_message(lang: str, key: str, **kwargs) -> str:
+    """Get localized message."""
+    messages = MESSAGES.get(lang, MESSAGES['fa'])
+    message = messages.get(key, MESSAGES['fa'].get(key, ''))
+    return message.format(**kwargs) if kwargs else message
